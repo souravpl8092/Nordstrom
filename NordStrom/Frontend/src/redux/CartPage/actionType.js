cd27af9a34291e1d692700dd916b75bc75ae252c
Sourav Paul <107460099+souravpl8092@users.noreply.github.com>
2023-04-17 10:21:30 +0530

    Delete NordStrom directory

--- NordStrom/Frontend/src/redux/CartPage/actionType.js
+++
@@ -1,6 +0,0 @@
-export const ADD_TO_CART = "cart/items/add/";
-export const REMOVE_FROM_CARTS = "cart/items/delete/";
-export const INCREMENT = "cart/items/inc/";
-export const DECREMENT = "cart/items/dec/";
-export const RESET = "cart/items/reset/";
-export const applyCoupon = "cart/applycoupon";
